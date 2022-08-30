7fa1305584a1f21948d7d0bb2ac506bb76292914
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-08-30 19:52:18 +0900

    Add files via upload

---
+++ PageCount.java
@@ -0,0 +1,27 @@
+package testBoard;
+
+public class PageCount {
+	int startNum;
+	int endNum;
+	int totPageNum;
+	
+	public int getStartNum() {
+		return startNum;
+	}
+	public void setStartNum(int startNum) {
+		this.startNum = startNum;
+	}
+	public int getEndNum() {
+		return endNum;
+	}
+	public void setEndNum(int endNum) {
+		this.endNum = endNum;
+	}
+	public int getTotPageNum() {
+		return totPageNum;
+	}
+	public void setTotPageNum(int totPageNum) {
+		this.totPageNum = totPageNum;
+	}
+	
+}
